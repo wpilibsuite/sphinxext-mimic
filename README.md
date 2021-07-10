663ebf787a7232f34ab4e66f44b522bd613101bc
Daltz333 <daltzsmith@gmail.com>
2021-07-09 22:26:47 -0400

    Initial Repo

---
+++ README.md
@@ -0,0 +1,30 @@
+# sphinxext-mimic
+![ci](https://github.com/wpilibsuite/sphinxext-mimictoc/workflows/ci/badge.svg)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+Sphinx Extension to create a fake expandable table of contents.
+
+## Installation
+
+`python -m pip install sphinxext-mimic`
+
+## Requirements
+
+- Sphinx >= 3
+
+## Usage
+Add `sphinxext.mimic` to your extensions list in your `conf.py`
+
+```python
+extensions = [
+   sphinxext.mimic,
+]
+```
+
+Then use
+
+```
+.. mimictoc::
+```
+
+which is compatible with the default `.. toctree::` arguments.
---
+++ sphinxext/mimictoc.py
@@ -0,0 +1,79 @@
+from sphinx.application import Sphinx
+from sphinx.util.docutils import SphinxDirective
+from sphinx import addnodes
+from docutils.nodes import Element, Node
+from sphinx.directives.other import TocTree
+
+from typing import Any, Dict, List, Set, Tuple, overload
+
+class mimictoc(TocTree):
+
+   # Override normal Sphinx ToC checking for warning removal
+   def parse_content(self, toctree: addnodes.toctree) -> List[Node]:
+        suffixes = self.config.source_suffix
+
+        # glob target documents
+        all_docnames = self.env.found_docs.copy()
+        all_docnames.remove(self.env.docname)  # remove current document
+
+        ret: List[Node] = []
+        excluded = Matcher(self.config.exclude_patterns)
+        for entry in self.content:
+            if not entry:
+                continue
+            # look for explicit titles ("Some Title <document>")
+            explicit = explicit_title_re.match(entry)
+            if (toctree['glob'] and glob_re.match(entry) and
+                    not explicit and not url_re.match(entry)):
+                patname = docname_join(self.env.docname, entry)
+                docnames = sorted(patfilter(all_docnames, patname))
+                for docname in docnames:
+                    all_docnames.remove(docname)  # don't include it again
+                    toctree['entries'].append((None, docname))
+                    toctree['includefiles'].append(docname)
+                if not docnames:
+                    ret.append(self.state.document.reporter.warning(
+                        'toctree glob pattern %r didn\'t match any documents'
+                        % entry, line=self.lineno))
+            else:
+                if explicit:
+                    ref = explicit.group(2)
+                    title = explicit.group(1)
+                    docname = ref
+                else:
+                    ref = docname = entry
+                    title = None
+                # remove suffixes (backwards compatibility)
+                for suffix in suffixes:
+                    if docname.endswith(suffix):
+                        docname = docname[:-len(suffix)]
+                        break
+                # absolutize filenames
+                docname = docname_join(self.env.docname, docname)
+                if url_re.match(ref) or ref == 'self':
+                    toctree['entries'].append((title, ref))
+                elif docname not in self.env.found_docs:
+                    if excluded(self.env.doc2path(docname, None)):
+                        message = 'toctree contains reference to excluded document %r'
+                    else:
+                        message = 'toctree contains reference to nonexisting document %r'
+
+                    ret.append(self.state.document.reporter.warning(message % docname,
+                                                                    line=self.lineno))
+                    self.env.note_reread()
+                else:
+                    all_docnames.remove(docname)
+
+                    toctree['entries'].append((title, docname))
+                    toctree['includefiles'].append(docname)
+
+        # entries contains all entries (self references, external links etc.)
+        if 'reversed' in self.options:
+            toctree['entries'] = list(reversed(toctree['entries']))
+            toctree['includefiles'] = list(reversed(toctree['includefiles']))
+
+        return ret
+
+
+def setup(app: Sphinx) -> Dict[str, Any]:
+   app.add_directive("mimictoc", mimictoc)
